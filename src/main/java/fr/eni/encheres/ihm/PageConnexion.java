47316b061f9d0062dd561d1dda67f71b0a54faff
apachaiz <dane.g.san@gmail.com>
2021-06-16 11:51:32 +0200

    Maj GUI

--- src/main/java/fr/eni/encheres/ihm/PageConnexion.java
+++ src/main/java/fr/eni/encheres/ihm/PageConnexion.java
@@ -1,11 +1,14 @@
 package fr.eni.encheres.ihm;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PageConnexion extends Fenetre {
     public static void main(String[] args) {
         Fenetre fenetre = new Fenetre();
+        fenetre.setVisible(true);
         fenetre.setTitle("Page de connexion");
+        fenetre.setSize(new Dimension(700, 750));
         fenetre.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
 }
--- src/main/java/fr/eni/encheres/ihm/PageCreerCompte.java
+++ src/main/java/fr/eni/encheres/ihm/PageCreerCompte.java
@@ -1,11 +1,14 @@
 package fr.eni.encheres.ihm;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PageCreerCompte extends FenetreDesktop {
     public static void main(String[] args) {
         Fenetre fenetre = new Fenetre();
+        fenetre.setVisible(true);
         fenetre.setTitle("Creation de compte");
+        fenetre.setSize(new Dimension(700, 750));
         fenetre.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
 }
